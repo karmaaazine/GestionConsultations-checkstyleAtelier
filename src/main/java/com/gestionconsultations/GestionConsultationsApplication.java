dd394f8c836e2aa4496300a97bf9581750598737
richdaleintern06 <richdaleintern06@gmail.com>
2025-11-14 17:17:28 +0100

    springboot infra

---
+++ src/main/java/com/gestionconsultations/GestionConsultationsApplication.java
@@ -0,0 +1,13 @@
+package com.gestionconsultations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConsultationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionConsultationsApplication.class, args);
+    }
+}
+
